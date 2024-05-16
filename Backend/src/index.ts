bdd939c63fa926454ea43716371819f9924acfd7
Nylast-bit <stalyn.fernandez27@gmail.com>
2024-05-16 09:26:14 -0400

    first commit

---
+++ Backend/src/index.ts
@@ -0,0 +1,45 @@
+// DEPENDENCIES
+import express from "express";
+import { Request, Response, NextFunction } from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import prisma from "../lib/prisma"
+
+
+// START SERVER APP
+const app = express();
+
+// ENABLE.ENV
+dotenv.config();
+
+// MIDDELWARES
+
+app.use(cookieParser())
+app.use(express.json());
+
+
+// MIDDELWARES ERROR HANDLER
+app.use((_err: any, _req: Request, res: Response, _next: NextFunction) => {
+    const status = _err.status || 500;
+    const message = _err.message || "Something went wrong!";
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
+});
+
+// APP LISTENING (PRISMA & DATABASE ERROR HANDLER)
+prisma.$connect()
+    .then(() => {
+        const PORT = process.env.PORT || 3001;
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}. Database connection successful.`);
+        });
+    })
+    .catch((error) => {
+        console.error("Unable to connect to the database:", error);
+        process.exit(1);
+    });
