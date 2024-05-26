fbc58e9169bd0ba0b00bf7f3fee4888ebf92f342
Nylast-bit <stalyn.fernandez27@gmail.com>
2024-05-26 01:46:29 -0400

    Cree los modelos, hice los metodos Create, Read and Readall

--- Backend/src/routes/book.routes.ts
+++ Backend/src/routes/book.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createBook } from "../controllers/book.controllers";
+import { createBook, getAllBooks, getBook } from "../controllers/book.controllers";
 
 const router = express.Router();
 
 router.post("/book", createBook)
+router.get("/book", getAllBooks)
+router.get("/book/:id", getBook)
 
 export default router;
