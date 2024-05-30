e3bfd14c718337a35a52db9f55302b7f27b8a2f7
ItsChenRD <75988266+ItsChenRD@users.noreply.github.com>
2024-05-30 14:03:00 -0400

    Arregle el problema que tenia que segun el zoom de la pagina los links <a> se desplazaban sin control por la pantalla

--- Front-End/src/pages/authentications/login/index.tsx
+++ Front-End/src/pages/authentications/login/index.tsx
@@ -15,15 +15,17 @@ function LogIn() {
                         <img id="imgCarrito_de_compras" src={CarritoCompras} />
                         <input id="Username" className="Username_y_Password" placeholder="Username" tabIndex={1} type='text' />
                         <input id="Password" className="Username_y_Password" placeholder="Password" tabIndex={2} type='text' />
-                        <div style={{ position: 'relative', top: '-3.3%', left: '-2%', display: 'flex', flexDirection: 'row' }}>
-                            <input type="checkbox" id="chbxRemenberMe" value="Remeber Me" />
-                            <h4 id="h4RemenberMe"> Remenber Me</h4>
-                            <a href="https://www.youtube.com" className='Links' style={{ position: 'relative', top: '29%', left: '39.5%', height: '40%', font: '1em Comic Sans MS', color: '#D1E9F9' }}>Forgot Password?</a>
+                        <div style={{ position: 'relative', alignItems: 'center', justifyContent: 'space-between', left: '-2.5%', marginTop: '-5%', display: 'flex', flexDirection: 'row', width: '89%', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                            <div style={{ display: 'flex', flexDirection: 'row'}}>
+                                <input type="checkbox" id="chbxRemenberMe" value="Remeber Me" />
+                                <h4 id="h4RemenberMe"> Remenber Me</h4>
+                            </div>
+                            <a href="https://www.youtube.com" className='Links' style={{ position: 'relative', height: '40%', maxHeight: '40%', font: '1em Oswald MS', color: '#D1E9F9', left: '-5%' }}>Forgot Password?</a>
                         </div>
                         <input id="LogIn" type='button' tabIndex={3} value='Log In' />
-                        <div id="divLinks">
-                            <a href="https://www.youtube.com" className='Links' style={{ color: '#D1E9F9' }}>Don’t have an account yet?</a>
-                            <a href="https://www.youtube.com" className='Links' style={{ color: '#1E608E' }}>Create an account</a>
+                        <div id="divLinks1">
+                            <a href="https://www.youtube.com" className='Links1' style={{ color: '#D1E9F9' }}>Don’t have an account yet?</a>
+                            <a href="https://www.youtube.com" className='Links1' style={{ color: '#1E608E' }}>Create an account</a>
                         </div>
                     </div>
                 </div>
