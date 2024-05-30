a8dd9d6979b8b911cd400f36dc5a4c0ce529d0d9
Jeaa30 <joseariasalmodovar@gmail.com>
2024-05-30 12:31:55 -0400

    cambios de estructura y recomendaciones del profesor

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,36 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Señorita from './assets/Señorita.png'
-import Libreria from './assets/SignUpFondo.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LogIn from './pages/authentications/login';
+import SignUp from './pages/authentications/singUp';
+import Home from './pages/authentications/home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-    <div className="container">
-      <img id="imgFondo" src={Libreria}/>
-      <div id="Controls" className="Controls">
-        <div id="Register" className="Register">
-          <h1 id="h1CreateAccount">Create Account</h1>
-          <img id="imgSeñorita" src={Señorita}/>
-          <div id="divName_y_Username">
-            <input id="Name" className="Name_y_Username" placeholder="Name" tabIndex={1} type='text'/>
-            <input id="Username" className="Name_y_Username" placeholder="Username" tabIndex={2} type='text'/>
-          </div>
-          <input id="Email" className="Email_y_Password" placeholder="Email" tabIndex={3} type='text'/>
-          <input id="Password" className="Email_y_Password" placeholder="Password" tabIndex={4} type='text'/>
-          <input id="SignUp" type='button' tabIndex={5} value='Sign Up'/>
-          <div id="divLinks">
-            <a href="https://www.youtube.com" className='Links' tabIndex={6} style={{ color: '#D1E9F9' }}>Already have an account?</a>
-            <a href="https://www.youtube.com" className='Links' tabIndex={7} style={{ color: '#1E608E' }}>Log in</a>
-          </div>
-        </div>
-      </div>
-      </div>
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<LogIn />} />
+
+          </ Route>
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </div>
   )
 }
 
---
+++ Front-End/src/pages/authentications/home/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import CarritoCompras from './assets/CarritoCompras.png'
+import Libreria from './assets/SignUpFondo.png'
+import './index.css'
+
+const Home = () => {
+
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Home
