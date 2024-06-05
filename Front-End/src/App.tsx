e4899ae89a63f164a244adeadf009446d01e3503
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 16:50:26 -0400

    validation

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import LogIn from "./pages/authentications/login";
 import SignUp from "./pages/authentications/signUp";
 import Home from "./pages/authentications/home";
+import Navbar from "./shared/layout/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route path="signup" element={<SignUp />} />
-          <Route path="login" element={<LogIn />} />
-          <Route path="home" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div style={{ height: "100%", width: "100%", display: "flex", margin: 0 }}>
+      <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="signup" element={<SignUp />} />
+            <Route path="login" element={<LogIn />} />
+            <Route path="home" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
