e53237c824dab9c22591099746808b316c5288a2
Jeaa30 <joseariasalmodovar@gmail.com>
2024-06-10 21:51:50 -0400

    Cambio de Idioma

--- Front-End/src/pages/authentications/MenuBook/index.tsx
+++ Front-End/src/pages/authentications/MenuBook/index.tsx
@@ -23,27 +23,27 @@ const MenuBook = () => {
                 </div>
                 <div id="divInferiorDerecho">
                     <div id="divContenedor">
-                        <div id="imgBack"/>
+                        <div id="imgBack" />
                         <div id="divLibro">
                             <div id="div-imgLibro">
                                 <div id="divCirculo_de_Mierda">
-                                    <img id="imgLibro"/>
+                                    <img id="imgLibro" />
                                 </div>
                             </div>
                             <div id="divInfoLibro">
                                 <h1 id="h1TituloLibro">El Principito</h1>
                                 <h2 id="h2Autor">Antoine de Saint-Exupéry</h2>
                                 <div id="divCalificación">
-                                    <div className="Star"/>
-                                    <div className="Star"/>
-                                    <div className="Star"/>
-                                    <div className="Star"/>
-                                    <div className="Star half"/>
+                                    <div className="Star" />
+                                    <div className="Star" />
+                                    <div className="Star" />
+                                    <div className="Star" />
+                                    <div className="Star half" />
                                 </div>
                                 <div id="div-h3Categoria">
-                                    <h3 id="h3Categoria">Children’s literature</h3>
+                                    <h3 id="h3Categoria">Literatura infantil</h3>
                                 </div>
-                                <h3 id="h3Info">The Little Prince, the protagonist of the story, is a very unique boy who lives on a distant asteroid, and who in his innocence is the bearer of great wisdom. He arrives on Earth after a long space journey, looking for someone who can draw a lamb.</h3>
+                                <h3 id="h3Info">El Principito, protagonista de la historia, es un niño muy singular que vive en un asteroide lejano y que, en su inocencia, es portador de una gran sabiduría. Llega a la Tierra tras un largo viaje espacial, buscando a alguien que sepa dibujar un cordero.</h3>
                                 <button id="btnComprar">Comprar</button>
                             </div>
                         </div>
